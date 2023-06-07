b91c64dda02b8e45daff479d36a1a3446599f852
Safrinadi Ilham <56718337+zafrielcc@users.noreply.github.com>
2023-06-07 16:22:00 +0700

    app.py

--- app.py
+++ app.py
@@ -309,4 +309,4 @@ def download_txt():
     return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=False)
